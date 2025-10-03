5489d17f292e9e5f00cc6fdbd79f882536bdb712
YANGZQ106 <yangzq21@gmail.com>
2025-10-04 00:42:19 +0800

    Update before-update-custom.sh

--- before-update-custom.sh
+++ before-update-custom.sh
@@ -11,4 +11,4 @@
 # echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >> ./feeds.conf.default   
 # echo "src-git small https://github.com/kenzok8/small" >> ./feeds.conf.default
 
- sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+ # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
